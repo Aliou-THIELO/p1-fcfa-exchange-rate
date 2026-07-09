5b40d70955b31d9ae4dfda14e26e842b2c214a67
Aliou THIELO <thieloaliou25@gmail.com>
2026-07-09 12:12:41 +0000

    Refactor to use requests.Session with retry strategy and timeout

--- src/ingest.py
+++ src/ingest.py
@@ -1,22 +1,33 @@
 import requests
 import os
 from dotenv import load_dotenv
+from urllib3.util import Retry
+from requests.adapters import HTTPAdapter
 
 load_dotenv()
-
 token = os.getenv("API_token")
 
-try :
-    response = requests.get(f"https://v6.exchangerate-api.com/v6/{token}/latest/USD")
-    response.raise_for_status()
-    result = response.json()
-    print(result)
+with requests.Session() as session:
+    retry = Retry(total = 3,
+              backoff_factor= 1,
+              status_forcelist=[500, 501,502 ,503])
+    adapter = HTTPAdapter (max_retries = retry)
+    session.mount("https://", adapter)
+    
+    try :
+        response = session.get(f"https://v6.exchangerate-api.com/v6/{token}/latest/USD", timeout= 10)
+        response.raise_for_status()
+        result = response.json()
+        print(result)
+    
+    except requests.exceptions.HTTPError as e:
+        print(f"Server error: {e}")
+    except requests.exceptions.ConnectionError as e:
+        print(f"Connection error: {e}")
+    except requests.exceptions.Timeout as e:
+        print(f"Server took too long to respond: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"Exception error: {e}")
 
-except requests.exceptions.HTTPError as e:
-    print(f"Server error: {e}")
-except requests.exceptions.ConnectionError as e:
-    print(f"Connection error: {e}")
-except requests.exceptions.Timeout as e:
-    print(f"Server took too long to respond: {e}")
-except requests.exceptions.RequestException as e:
-    print(f"Exception error: {e}")
+
+
