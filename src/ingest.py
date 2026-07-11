7850f0f670217416537718540f5222e1158cdc50
Aliou THIELO <thieloaliou25@gmail.com>
2026-07-11 19:12:05 +0000

    Add custom exception hierarchy for API errors

--- src/ingest.py
+++ src/ingest.py
@@ -8,6 +8,16 @@ load_dotenv()
 token = os.getenv("API_token")
 
 
+class APIError(Exception):
+    pass
+class AuthError(APIError):
+    pass
+class NotFoundError(APIError):
+    pass
+class ServerError(APIError):
+    pass
+
+
 class ExchangeRateClient:
 
     def __init__(self, base: str, token: str):
@@ -30,18 +40,35 @@ class ExchangeRateClient:
             return response.json()
 
         except requests.exceptions.HTTPError as e:
-            print(f"Server error: {e}")
-        except requests.exceptions.ConnectionError as e:
-            print(f"Connection error: {e}")
-        except requests.exceptions.Timeout as e:
-            print(f"Server took too long to respond: {e}")
-        except requests.exceptions.RequestException as e:
-            print(f"Exception error: {e}")
+            code = e.response.status_code
+            if 400 <= code <= 403:
+                raise AuthError(f"Access denied: {code}") from e
+            elif code == 404:
+                raise NotFoundError(f"Resource not found: {code}") from e
+            elif 500 <= code < 600:
+                raise ServerError(f"Server error: {code}") from e
+            else:
+                raise APIError(f"Unexpected API error: {code}") from e
+        except requests.exceptions.Timeout:
+            raise APIError(f"Timeout.") from None
+        except requests.exceptions.ConnectionError:
+            raise APIError(f"Connection error.") from None
 
     def get_latest_rates(self, base_currency: str):
         return self._appel(f"latest/{base_currency}", param={})
-    
+
+
 if __name__ == "__main__":
-    client = ExchangeRateClient("https://v6.exchangerate-api.com/v6", token)
-    result = client.get_latest_rates("USD")
-    print(result)
+    try:
+        client = ExchangeRateClient("https://v6.exchangerate-api.com/v6", token)
+        result = client.get_latest_rates("USD")
+        print(result)
+
+    except AuthError as e:
+        print(f"Error Authorization: {e}")
+    except NotFoundError as e:
+        print(f"Not found: {e}")
+    except ServerError as e:
+        print(f"Server error: {e}")
+    except APIError as e:
+        print(f"API error: {e}")
